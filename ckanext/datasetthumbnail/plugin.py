f881ba9de5d924c5bf1fd360c7edc19591008e65
Alan M Jackson <alan@aptivate.org>
2016-05-27 15:35:06 +0100

    Initial version of plugin. Doesn't work yet but returns a url for our logo

---
+++ ckanext/datasetthumbnail/plugin.py
@@ -0,0 +1,45 @@
+import pylons.config as config
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+def thumbnail_url():
+    '''Returns the url of a thumbnail for a dataset. 
+
+    Looks for a resource "thumbnail.png" in a dataset. 
+    Will generate a thumbnail and save it to the dataset if there isn't one already. 
+
+    To change this setting add to the
+    [app:main] section of your CKAN config file::
+
+      ckan.datasetthumbnail.show_thumbnail = true
+
+    :rtype: string
+    '''
+    show_thumbnail = config.get('ckan.datasetthumbnail.show_thumbnail', False)
+    if not show_thumbnail:
+        return None
+
+    return 'http://www.aptivate.org/static/images/aptivate-logo.png'
+
+
+
+
+
+class DatasetthumbnailPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'datasetthumbnail')
+
+        #ITemplateHelpers
+    def get_helpers(self):
+        return {
+            'thumbnail_url': thumbnail_url
+        }
