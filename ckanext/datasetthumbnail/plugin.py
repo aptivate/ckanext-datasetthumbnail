6e540dd2e202ad20df293faf63cc41fe32e8598c
Martin Burchell <martinb@aptivate.org>
2016-06-02 10:43:44 +0100

    Experimental change to stream image

--- ckanext/datasetthumbnail/plugin.py
+++ ckanext/datasetthumbnail/plugin.py
@@ -10,10 +10,10 @@ from StringIO import StringIO
 
 
 def thumbnail_url(package):
-    '''Returns the url of a thumbnail for a dataset. 
+    '''Returns the url of a thumbnail for a dataset.
 
-    Looks for a resource "thumbnail.png" in a dataset. 
-    Will generate a thumbnail and save it to the dataset if there isn't one already. 
+    Looks for a resource "thumbnail.png" in a dataset.
+    Will generate a thumbnail and save it to the dataset if there isn't one already.
 
     To change this setting add to the
     [app:main] section of your CKAN config file::
@@ -22,6 +22,7 @@ def thumbnail_url(package):
 
     :rtype: string
     '''
+
     value = config.get('ckan.datasetthumbnail.show_thumbnail', False)
     show_thumbnail = toolkit.asbool(value)
     thumbnail_width = config.get('ckan.datasetthumbnail.thumbnail_width', 140)
@@ -40,25 +41,34 @@ def thumbnail_url(package):
     for resource in package['resources']:
         if resource['format'] == 'JPEG':
 
-            response = requests.get(resource['url'])
-            image = Image.open(StringIO(response.content))
-            image.thumbnail((thumbnail_width, thumbnail_height))
+            response = requests.get(resource['url'], stream=True)
+            if response.status_code == 200:
+                original_fp = tempfile.NamedTemporaryFile(delete=False)
 
-            #fp = StringIO() #create an in-memory file object in which to save the thumbnail image
-            fp = tempfile.NamedTemporaryFile()
-            image.save(fp, 'PNG')
+                for chunk in response.iter_content(1024):
+                    original_fp.write(chunk)
+                original_fp.flush()
 
-            thumbnail_resource = {}
-            thumbnail_resource['package_id'] = package['id']
-            thumbnail_resource['url'] = 'thumbnail.png'
-            thumbnail_resource['url_type'] = 'upload'
-            thumbnail_resource['format'] = 'png'
-            thumbnail_resource['name'] = 'thumbnail.png'
-            thumbnail_resource['upload'] = _UploadLocalFileStorage(fp)
-       
-            created_resource = toolkit.get_action('resource_create')(data_dict=thumbnail_resource)
-            fp.close()
-            return created_resource['url']
+                image = Image.open(original_fp.name)
+                image.thumbnail((thumbnail_width, thumbnail_height))
+
+                #fp = StringIO() #create an in-memory file object in which to save the thumbnail image
+                thumbnail_fp = tempfile.NamedTemporaryFile()
+                image.save(thumbnail_fp, format='PNG')
+
+                thumbnail_resource = {}
+                thumbnail_resource['package_id'] = package['id']
+                thumbnail_resource['url'] = 'thumbnail.png'
+                thumbnail_resource['url_type'] = 'upload'
+                thumbnail_resource['format'] = 'png'
+                thumbnail_resource['name'] = 'thumbnail.png'
+                thumbnail_resource['upload'] = _UploadLocalFileStorage(thumbnail_fp)
+
+                created_resource = toolkit.get_action('resource_create')(data_dict=thumbnail_resource)
+                thumbnail_fp.close()
+                original_fp.close()
+
+                return created_resource['url']
 
     return '/image-icon.png'
 
